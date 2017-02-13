b229d2546597a67b2fd340bf198f8da48d0b0193
Agustin Carrasco <asermax@gmail.com>
2017-02-13 00:02:15 -0300

    build: use port 80 on dev server to make routing easier on nginx

--- webpack.config.js
+++ webpack.config.js
@@ -29,7 +29,7 @@ module.exports = function(env) {
       hotOnly: true,
       contentBase: resolve(__dirname, 'src'),
       host: '0.0.0.0',
-      port: '8000',
+      port: '80',
       publicPath: '/',
     },
 
