64696dffd0ba5cf330bc02f4b1d9d29a705726a2
Agustin Carrasco <asermax@gmail.com>
2017-02-18 20:52:48 -0300

    chore: renamed modules to pages

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Route } from 'react-router';
-import App from './modules/App';
+import App from './pages/App';
 
 export default (
   <Route path="/" component={App}>
