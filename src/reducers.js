13436f348e2f3c1cbac7a476412eb9f801155c40
Agustin Carrasco <asermax@gmail.com>
2017-02-24 01:58:49 -0300

    reducers: setup a better structure for reducers

--- src/reducers.js
+++ src/reducers.js
@@ -1,7 +1,9 @@
-import { combineReducers } from 'redux';
+import { combineReducers } from 'redux'
 import { routerReducer } from 'react-router-redux'
+import showReducers from './data/show/reducers'
 
 // Combine all reducers into one root reducer
 export default combineReducers({
   routing: routerReducer,
-});
+  shows: showReducers,
+})
