7fc286749b44c85ed16ef94bc262718a0d1971e9
Agustin Carrasco <asermax@gmail.com>
2017-02-28 17:21:30 -0300

    show: added simple components for show and show list

---
+++ src/components/show/Show/index.jsx
@@ -0,0 +1,20 @@
+import React, { PropTypes } from 'react'
+import styles from './styles.scss'
+
+const Show = (props) => (
+  <div className={styles.show}>
+    <div className={styles.showTitle}>
+      {props.name}
+    </div>
+    <img className={styles.showImage} src={props.image} />
+  </div>
+)
+
+Show.propTypes = {
+  shows: PropTypes.arrayOf(PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    image: PropTypes.string.isRequired,
+  })),
+}
+
+export default Show
---
+++ src/components/show/ShowList/index.jsx
@@ -0,0 +1,27 @@
+import React, { PropTypes } from 'react'
+import Show from 'components/show/Show'
+import styles from './styles.scss'
+
+const ShowList = (props) => (
+  <div className={styles.showList}>
+    {
+      props.shows.map(show => (
+        <Show
+          name={show.name}
+          image={show.image}
+          key={show._id}
+        />
+      ))
+    }
+  </div>
+)
+
+ShowList.propTypes = {
+  shows: PropTypes.arrayOf(PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    image: PropTypes.string.isRequired,
+    _id: PropTypes.number.isRequired,
+  })).isRequired,
+}
+
+export default ShowList
