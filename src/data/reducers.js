9ed6d88ca67d25807431f6c00f73a72bda990705
Agustin Carrasco <asermax@gmail.com>
2017-03-01 00:04:06 -0300

    reducer: missed reference to react-redux-router

--- src/data/reducers.js
+++ src/data/reducers.js
@@ -1,9 +1,7 @@
 import { combineReducers } from 'redux'
-import { routerReducer } from 'react-router-redux'
 import showReducers from './show/reducers'
 
 // Combine all reducers into one root reducer
 export default combineReducers({
-  routing: routerReducer,
   shows: showReducers,
 })
