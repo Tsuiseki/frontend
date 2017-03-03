ae90a68f262c85d080a4eedd11f7e51ecc2814b3
Agustin Carrasco <asermax@gmail.com>
2017-03-03 01:03:56 -0300

    api: use async-await instead of promise

--- src/data/api.js
+++ src/data/api.js
@@ -1,9 +1,9 @@
 import _fetch from 'isomorphic-fetch'
 const API_PREFIX = 'api/v1'
 
-function fetch(entrypoint) {
-  return _fetch(`${API_PREFIX}/${entrypoint}`)
-    .then(response => response.json())
+async function fetch(entrypoint) {
+  const response = await _fetch(`${API_PREFIX}/${entrypoint}`)
+  return response.json()
 }
 
 export default {
