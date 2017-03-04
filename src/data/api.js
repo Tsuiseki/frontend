1a89672ac649a270991f3ee5494e139e73ce41e6
Agustin Carrasco <asermax@gmail.com>
2017-03-04 20:49:23 -0300

    api: added create function

--- src/data/api.js
+++ src/data/api.js
@@ -1,11 +1,30 @@
 import _fetch from 'isomorphic-fetch'
 const API_PREFIX = 'api/v1'
 
+function buildPath(entrypoint) {
+  return `${API_PREFIX}/${entrypoint}`
+}
+
 async function fetch(entrypoint) {
-  const response = await _fetch(`${API_PREFIX}/${entrypoint}`)
+  const response = await _fetch(buildPath(entrypoint))
+  return response.json()
+}
+
+async function create(entrypoint, data) {
+  const response = await _fetch(
+    buildPath(entrypoint),
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    }
+  )
   return response.json()
 }
 
 export default {
   fetch,
+  create,
 }
