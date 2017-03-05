7387548aa380f7bf86483db6606c69f39d594b27
Agustin Carrasco <asermax@gmail.com>
2017-03-04 21:32:54 -0300

    show: added simple edit component

---
+++ src/components/show/ShowEdit/index.jsx
@@ -0,0 +1,42 @@
+import React, { PropTypes } from 'react'
+import styles from './styles.scss'
+
+const ShowEdit = (props) => {
+  let nameInput, episodesInput, imageInput
+
+  return (
+    <form
+      className={styles.showEdit}
+      onSubmit={(event) => {
+        event.preventDefault()
+        props.onSubmit({
+          name: nameInput.value,
+          image: imageInput.value,
+          episodes: episodesInput.value,
+        })
+      }}
+    >
+      <div>
+        <label>Name: </label>
+        <input type="text" ref={node => nameInput = node}/>
+      </div>
+      <div>
+        <label>Image: </label>
+      <input type="text" ref={node => imageInput = node}/>
+      </div>
+      <div>
+        <label>Episodes: </label>
+        <input type="text" ref={node => episodesInput = node}/>
+      </div>
+      <button type="submit">
+        Create
+      </button>
+    </form>
+  )
+}
+
+ShowEdit.propTypes = {
+  onSubmit: PropTypes.func.isRequired,
+}
+
+export default ShowEdit
