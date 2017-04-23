a63607debb3067981ac8cf9a7c9d8a2bfd20fc5b
Agustin Carrasco <asermax@gmail.com>
2017-04-23 20:58:32 -0300

    refactor: use prop-types instead of built-in react's PropTypes

--- src/pages/ShowGallery/index.jsx
+++ src/pages/ShowGallery/index.jsx
@@ -1,4 +1,5 @@
-import React, { Component, PropTypes } from 'react'
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import { getShows } from 'data/show/selectors'
 import { fetchShows, createShow, deleteShow } from 'data/show/actions'
