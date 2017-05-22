160cd990fe0d1547bd6bc027c7f304d86b5ef665
Agustin Carrasco <asermax@gmail.com>
2017-05-21 22:08:24 -0300

    show edit: refactor to handle state changes

--- src/components/show/ShowEdit/index.jsx
+++ src/components/show/ShowEdit/index.jsx
@@ -1,45 +1,68 @@
-import React from 'react'
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import styles from './styles.scss'
+import { CREATION } from 'data/show/states'
 
-const ShowEdit = (props) => {
-  let nameInput, episodesInput, imageInput
+class ShowEdit extends Component {
+  constructor(props) {
+    super(props)
 
-  return (
-    <form
-      className={styles.showEdit}
-      onSubmit={(event) => {
-        event.preventDefault()
-        props.onSubmit(
-          {
-            name: nameInput.value,
-            episodes: episodesInput.value,
-          },
-          imageInput.files[0],
-        )
-      }}
-    >
-      <div>
-        <label>Name: </label>
-        <input type="text" ref={node => nameInput = node}/>
-      </div>
-      <div>
-        <label>Image: </label>
-      <input type="file" ref={node => imageInput = node}/>
-      </div>
-      <div>
-        <label>Episodes: </label>
-        <input type="text" ref={node => episodesInput = node}/>
-      </div>
-      <button type="submit">
-        Create
-      </button>
-    </form>
-  )
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (this.props.creationState === CREATION.IN_PROGRESS &&
+        nextProps.creationState === CREATION.SUCCEEDED) {
+      this.cleanInputs()
+    }
+  }
+
+  cleanInputs() {
+    this.nameInput.value = ''
+    this.episodesInput.value = ''
+    this.imageInput.value = ''
+  }
+
+  handleSubmit(event) {
+    event.preventDefault()
+    this.props.onSubmit(
+      {
+        name: this.nameInput.value,
+        episodes: this.episodesInput.value,
+      },
+      this.imageInput.files[0],
+    )
+  }
+
+  render() {
+    return (
+      <form
+        className={styles.showEdit}
+        onSubmit={(event) => this.handleSubmit(event)}
+      >
+        <div>
+          <label>Name: </label>
+          <input type="text" ref={node => this.nameInput = node}/>
+        </div>
+        <div>
+          <label>Image: </label>
+        <input type="file" ref={node => this.imageInput = node}/>
+        </div>
+        <div>
+          <label>Episodes: </label>
+          <input type="text" ref={node => this.episodesInput = node}/>
+        </div>
+        <button type="submit" disabled={this.props.creationState === CREATION.IN_PROGRESS}>
+          Create
+        </button>
+      </form>
+    )
+  }
 }
 
 ShowEdit.propTypes = {
   onSubmit: PropTypes.func.isRequired,
+  creationState: PropTypes.string.isRequired,
 }
 
 export default ShowEdit
