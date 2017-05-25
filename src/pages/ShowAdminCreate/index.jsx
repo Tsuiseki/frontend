daed4f87da17350963ffefa34c93336f1d53fc40
Agustin Carrasco <asermax@gmail.com>
2017-05-25 02:47:13 -0300

    show admin: move the logic for creation to the create page

--- src/pages/ShowAdminCreate/index.jsx
+++ src/pages/ShowAdminCreate/index.jsx
@@ -1,22 +1,73 @@
-import React from 'react'
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { push } from 'connected-react-router'
 import { connect } from 'react-redux'
 import { getCreationState } from 'data/show/selectors'
 import { createShow } from 'data/show/actions'
+import { CREATION } from 'data/show/states'
 import ShowEdit from 'components/show/ShowEdit'
 import styles from './styles.scss'
 
-const ShowAdminCreate = (props) => {
-  return (
-    <div className={styles.showAdminCreate}>
-      <ShowEdit
-        onSubmit={(show, image) => props.createShow(show, image)}
-        onCancel={props.goBack}
-        creationState={props.creationState}
-      />
-    </div>
-  )
+class ShowAdminCreate extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      name: '',
+      episodes: null,
+      image: {
+        value: null,
+        file: null,
+      },
+    }
+
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.creationState === CREATION.SUCCEEDED) {
+      this.setState({
+        name: '',
+        episodes: null,
+        image: {
+          value: null,
+          file: null,
+        },
+      })
+    }
+  }
+
+  handleChange(change) {
+    this.setState(change)
+  }
+
+  handleSubmit() {
+    this.props.createShow(
+      {
+        name: this.state.name,
+        episodes: this.state.episodes,
+      },
+      this.state.image.file,
+    )
+  }
+
+  render() {
+    return (
+      <div className={styles.showAdminCreate}>
+        <ShowEdit
+          onChange={this.handleChange}
+          onSubmit={this.handleSubmit}
+          onCancel={this.props.goBack}
+          isSaving={this.props.creationState === CREATION.IN_PROGRESS}
+          show={{
+            name: this.state.name,
+            image: this.state.image.value,
+            episodes: this.state.episodes,
+          }}
+        />
+      </div>
+    )
+  }
 }
 
 ShowAdminCreate.propTypes = {
